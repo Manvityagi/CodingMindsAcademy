8238b1f01f5b669eb2481a339f060ba9b8b5eb7a
Manvi Tyagi <manvityagi770@gmail.com>
2020-04-01 20:26:13 +0530

    Updates

---
+++ precache-manifest.6f7b5695ede2296e2a56e2cdd812a772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c565dd7f95e068963e62c4db8631768d",
+    "url": "/CodingMindsAcademy/index.html"
+  },
+  {
+    "revision": "d71a87af4cafd8fd7dfb",
+    "url": "/CodingMindsAcademy/static/css/main.5cdf4a41.chunk.css"
+  },
+  {
+    "revision": "b1b56ce37c76ad36835b",
+    "url": "/CodingMindsAcademy/static/js/2.346ce002.chunk.js"
+  },
+  {
+    "revision": "e85e620f3fcc49b161ee1b95d282124d",
+    "url": "/CodingMindsAcademy/static/js/2.346ce002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d71a87af4cafd8fd7dfb",
+    "url": "/CodingMindsAcademy/static/js/main.75c4f2b4.chunk.js"
+  },
+  {
+    "revision": "64cdf6617fad0ac4f498",
+    "url": "/CodingMindsAcademy/static/js/runtime-main.be37f1d0.js"
+  },
+  {
+    "revision": "3cd0fcb7de8c848e9f529e9621a44628",
+    "url": "/CodingMindsAcademy/static/media/finalLogo.3cd0fcb7.jpeg"
+  }
+]);
